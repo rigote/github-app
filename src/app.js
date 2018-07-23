3d60c922376fe02b90e44a70598575e2c8797e19
Rigote <matheus.rigote@rakuten.com.br>
2018-07-23 19:36:08 -0300

    Realizando a marcação do projeto

--- src/app.js
+++ src/app.js
@@ -1,12 +1,44 @@
 'use strict'
 
 import React, {Component} from 'react'
-import Timer from './timer'
 
 class App extends Component {
   render(){
     return(
-      <div>
+      <div className='app'>
+        <div className='search'>
+          <input type='search' placeholder='Digite o nome do usuário no Github.'/>
+        </div>
+
+        <div className='user-info'>
+          <img src='https://avatars0.githubusercontent.com/u/10486867?v=4' />
+          <h1><a href='https://api.github.com/users/rigote'>Matheus Souza Rigote</a></h1>
+
+          <ul className='repos-info'>
+            <li>Repositórios: 10</li>
+            <li>Seguidores: 0</li>
+            <li>Seguindo: 0</li>
+          </ul>
+
+          <div className='actions'>
+            <button>Ver repositórios</button>
+            <button>Ver favoritos</button>
+          </div>
+
+          <div className='repos'>
+            <h2>Repositórios:</h2>
+            <ul>
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+
+          <div className='starred'>
+            <h2>Favoritos:</h2>
+            <ul>
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
