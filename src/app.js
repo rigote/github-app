a08983103252143bce2bb9bbc1cf2c75940def08
Rigote <matheus.rigote@rakuten.com.br>
2018-07-23 19:17:50 -0300

    Configurando Projeto

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, {Component} from 'react'
+import Timer from './timer'
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App
